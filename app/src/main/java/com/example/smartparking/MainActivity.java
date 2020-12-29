2e89763d7d2e9a316de16817a52c3a536af5ad3b
engr <zamenya98@gmail.com>
2020-12-29 21:39:44 +0300

    functional

--- app/src/main/java/com/example/smartparking/MainActivity.java
+++ app/src/main/java/com/example/smartparking/MainActivity.java
@@ -1,34 +1,25 @@
 package com.example.smartparking;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -129,7 +120,8 @@ public class MainActivity extends AppCompatActivity {
                                  }
 
 
-        );}
+        );
+    }
 
     /**
      * Shows the progress UI and hides the login form.
