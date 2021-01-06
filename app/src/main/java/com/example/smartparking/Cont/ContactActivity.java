109940ae15e020d30d565632ec2dd7aa9e9ccd16
engr <zamenya98@gmail.com>
2021-01-07 00:48:24 +0300

    Added about

--- app/src/main/java/com/example/smartparking/Cont/ContactActivity.java
+++ app/src/main/java/com/example/smartparking/Cont/ContactActivity.java
@@ -1,16 +1,39 @@
 package com.example.smartparking.Cont;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.smartparking.R;
+import com.example.smartparking.SecondActivity;
 
 public class ContactActivity extends AppCompatActivity {
+    private ImageView btn_backHome;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        btn_backHome= findViewById(R.id.iv_back);
+        progressBar= findViewById(R.id.simpleProgressBar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        btn_backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                startActivity(new Intent(getApplicationContext(), SecondActivity.class));
+                finish();
+            }
+        });
     }
 }
