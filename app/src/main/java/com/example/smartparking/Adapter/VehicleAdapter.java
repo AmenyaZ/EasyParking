14c39b0d84dd578e5dd2f1d64cf12ed6f1d9b5b4
engr <zamenya98@gmail.com>
2021-01-07 23:34:41 +0300

    Adding Database

---
+++ app/src/main/java/com/example/smartparking/Adapter/VehicleAdapter.java
@@ -0,0 +1,109 @@
+package com.example.smartparking.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.smartparking.R;
+import com.example.smartparking.VehicleDetails.VehicleDetails;
+import com.example.smartparking.model.VehicleModel;
+
+import java.util.List;
+
+public class VehicleAdapter extends RecyclerView.Adapter<VehicleAdapter.WorkerViewHolder> {
+    private static final String TAG = "productAdapter";
+    List<VehicleModel> mechanicList;
+    private Context mcontext;
+
+    public VehicleAdapter(Context context, List<VehicleModel> list) {
+
+        mcontext = context;
+        mechanicList = list;
+    }
+
+    @NonNull
+    @Override
+    public WorkerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: new View...");
+        View view = LayoutInflater.from(mcontext).inflate(R.layout.recycler_layout, parent, false);
+        WorkerViewHolder mechanicView = new WorkerViewHolder(view);
+
+        Log.d(TAG, "onCreateViewHolder: view created...");
+        return mechanicView;
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull WorkerViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: align to recycler...");
+        final VehicleModel currentMechanic = mechanicList.get(position);
+        holder.recName.setText("Name : " + currentMechanic.getName());
+        holder.recLocation.setText("Location : " + currentMechanic.getLocation());
+        holder.recSpec.setText("Specialization : " + currentMechanic.getSpeciality());
+//        holder.recMail.setText("Email : " + currentMechanic.getEmail());
+        Picasso.get()
+                .load(currentMechanic.getImageUrl())
+                .placeholder(R.drawable.ic_image_black_24dp)
+                .fit()
+                .centerCrop()
+                .into(holder.recImage);
+
+
+        holder.clickedLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent passIntent = new Intent(mcontext, VehicleDetails.class);
+                passIntent.putExtra("name", currentMechanic.getName());
+                passIntent.putExtra("location", currentMechanic.getLocation());
+                passIntent.putExtra("mail", currentMechanic.getEmail());
+                passIntent.putExtra("image", currentMechanic.getImageUrl());
+                passIntent.putExtra("phone", currentMechanic.getPhone());
+                passIntent.putExtra("speciality", currentMechanic.getSpeciality());
+
+                mcontext.startActivity(passIntent);
+                Log.d(TAG, "onClick: detail view...");
+            }
+        });
+
+        Log.d(TAG, "onBindViewHolder: done binding....");
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: Counting method...");
+        if (mechanicList != null) {
+            Log.d(TAG, "getItemCount: Done counting for list...");
+            return mechanicList.size();
+        }
+        Log.d(TAG, "getItemCount: Done counting for non-list...");
+        return 0;
+    }
+
+    public class WorkerViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView recImage;
+        TextView recName, recLocation, recSpec, recMail;
+        LinearLayout clickedLayout;
+
+        public WorkerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Log.d(TAG, "productViewHolder: Initializing recycler items...");
+            recImage = itemView.findViewById(R.id.recycleImage);
+            recName = itemView.findViewById(R.id.recycleName);
+            recLocation = itemView.findViewById(R.id.recycleLocation);
+            recSpec = itemView.findViewById(R.id.recycleSpeciality);
+//            recMail = itemView.findViewById(R.id.recycleEmail);
+            clickedLayout = itemView.findViewById(R.id.rec);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/smartparking/VehicleProfile/VehicleProf.java
@@ -0,0 +1,283 @@
+package com.example.smartparking.VehicleProfile;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartparking.R;
+import com.example.smartparking.VehicleDetails.VehicleDetails;
+import com.example.smartparking.model.VehicleModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class VehicleProf extends AppCompatActivity {
+
+    EditText name, phone, location, email;
+    Uri image_uri;
+    Button uploadButton ;
+    ImageView workImage;
+    String nameWork, phoneWork, locationWork, emailWork, spec;
+    FirebaseDatabase database ;
+    DatabaseReference mechanicReference ;
+    com.example.smartparking.model.VehicleModel worker ;
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+    private ProgressBar mProgress;
+    public static final int IMAGE_REQUEST = 1;
+    private static final int PERMISSION_CODE = 1000;
+    private AutoCompleteTextView autoCompleteTextView;
+
+    private static final int CAMERA_REQUEST_CODE =1;
+
+    // private StorageReference mStorage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_prof);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        setTitle("New Mechanic");
+
+//        Drop down list of mechanics
+        String [] specification = new String[] {"Service Technicians", "Diagnostic Technicians", "Brake and Transmission Technicians", "Body Repair Technicians",
+                "Vehicle Refinishers", "Vehicle Inspectors", "General"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown, specification);
+
+        autoCompleteTextView = findViewById(R.id.specList);
+        autoCompleteTextView.setAdapter(adapter);
+
+        storageReference = FirebaseStorage.getInstance().getReference("Mechanics");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Mechanics");
+//        database = FirebaseDatabase.getInstance();
+//        mechanicReference = database.getReference("Mechanics");
+        name = findViewById(R.id.etWorkUserName);
+        phone = findViewById(R.id.etWorkPhone);
+        location = findViewById(R.id.etWorkLocation);
+        email = findViewById(R.id.etWorkMail);
+        uploadButton = findViewById(R.id.uploadDBtn);
+        workImage = findViewById(R.id.imageViewWorker);
+//        imageBtn = findViewById(R.id.imageDBtn);
+        mProgress = findViewById(R.id.progressBar);
+
+
+        worker =new VehicleModel();
+
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                receiveEntries();
+            }
+        });
+
+
+    }
+
+    private void uploadDetails() {
+        if (image_uri != null) {
+            uploadInfo();
+        } else {
+            Toast.makeText(this, "Empty Uri...Select an Image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadInfo() {
+
+
+        StorageReference photoReference = storageReference.child(System.currentTimeMillis() + "."
+                + getFileExtension(image_uri));
+
+        UploadTask uploadTask = photoReference.putFile(image_uri);
+
+        Toast.makeText(this, "UP " + uploadTask, Toast.LENGTH_SHORT).show();
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+                // Handle unsuccessful uploads
+                Toast.makeText(VehicleDetails.this, "Fail...", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
+
+                Toast.makeText(VehicleDetails.this, "Success...", Toast.LENGTH_LONG).show();
+
+                if (taskSnapshot.getMetadata() != null) {
+                    if (taskSnapshot.getMetadata().getReference() != null) {
+                        Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                        result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Toast.makeText(VehicleDetails.this, "Proceed...", Toast.LENGTH_LONG).show();
+                                String sImage = uri.toString();
+
+                                mProgress.setVisibility(View.VISIBLE);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mProgress.setProgress(0);
+                                    }
+                                }, 500);
+                                Toast.makeText(VehicleDetails.this, "Upload Successful..." + sImage, Toast.LENGTH_SHORT).show();
+
+                                worker = new VehicleDetails(nameWork, phoneWork, locationWork, emailWork, sImage, spec);
+                                String key = databaseReference.push().getKey();
+                                worker.setId(key);
+                                databaseReference.child(key).setValue(worker);
+
+                                Toast.makeText(VehicleDetails.this, "Success Key retention...", Toast.LENGTH_LONG).show();
+                                mProgress.setVisibility(View.INVISIBLE);
+                                backToProfile(nameWork, phoneWork,locationWork, emailWork, sImage, spec);
+                                name.setText("");
+                                location.setText("");
+                                phone.setText("");
+                                email.setText("");
+                                Picasso.get().load("null").placeholder(R.drawable.ic_image_black_24dp).into(workImage);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                mProgress.setVisibility(View.INVISIBLE);
+                                Toast.makeText(VehicleDetails.this, "Database Fail...", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                }
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                mProgress.setProgress((int) progress);
+            }
+        });
+
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        String extension = mime.getExtensionFromMimeType(contentResolver.getType(uri));
+        return extension;
+    }
+
+    private void backToProfile(String nameWork, String phoneWork, String locationWork, String emailWork, String imageWork, String special) {
+        Intent backIntent = new Intent(this, Profile.class);
+        backIntent.putExtra("phone", phoneWork);
+        backIntent.putExtra("name", nameWork);
+        backIntent.putExtra("location", locationWork);
+//        backIntent.putExtra("price", pro;
+//        backIntent.putExtra("capacity", product.getCapacity());
+        backIntent.putExtra("mail", emailWork);
+        backIntent.putExtra("image", imageWork);
+        backIntent.putExtra("speciality", special);
+//        backIntent.putExtra("key", key);
+        Toast.makeText(this, "Successful Upload of Details..", Toast.LENGTH_SHORT).show();
+//
+        startActivity(backIntent);
+//        Toast.makeText(this, "Upload Done Go to Profile", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void receiveEntries() {
+        nameWork = name.getText().toString().trim();
+        phoneWork = phone.getText().toString().trim();
+        locationWork = location.getText().toString().trim();
+        emailWork = email.getText().toString().trim();
+        spec = autoCompleteTextView.getText().toString().trim();
+
+        checkFields();
+    }
+
+    private void checkFields() {
+        if (nameWork.isEmpty() || phoneWork.isEmpty() || locationWork.isEmpty() || emailWork.isEmpty() || image_uri == null
+                || spec.isEmpty()) {
+            Toast.makeText(this, "Missing Fields...", Toast.LENGTH_SHORT).show();
+        } else {
+
+            if ((phoneWork.length()) < 10) {
+                Toast.makeText(this, "Please Enter a Valid Phone Number", Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//                    if (Integer.valueOf(emailMech) ) {
+//                        Toast.makeText(this, "Please Enter a Valid Capacity", Toast.LENGTH_SHORT).show();
+            }
+            else {
+
+                if (isNetworkConnected()) {
+                    uploadDetails();
+                }
+
+                else {
+                    Toast.makeText(WorkerProfile.this, R.string.No_network, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+
+    }
+
+    private void choosingPhoto() {
+        Intent chooseIntent = new Intent();
+        chooseIntent.setType("image/*");
+        chooseIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(chooseIntent, IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_REQUEST) {
+
+                image_uri = data.getData();
+                Picasso.get().load(image_uri).into(workImage);
+
+            }
+        }
+    }
+
+    public void selectPhoto(View view) {
+        choosingPhoto();
+    }
+}
